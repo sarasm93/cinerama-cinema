8eac43a720a76d48fe6f34afc888df6829e1dc61
Sara Smith <saraa.smith@hotmail.com>
2023-06-28 12:02:25 +0000

    Change registration template to render form as template variable instead of html

--- cinemasite/forms.py
+++ cinemasite/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Customer
+from django.forms import TextInput
 
 
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'address', 'email', 'phone_number', 'password']
+        widgets = {
+            'phone_number': TextInput
+        }
