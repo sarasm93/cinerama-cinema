8964997c8431d5472a11c666b2d8db953328398f
Sara Smith <saraa.smith@hotmail.com>
2023-07-12 07:26:27 +0000

    Change debug mode

--- cinerama/settings.py
+++ cinerama/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cinerama-cinema-4c2cf705bc92.herokuapp.com', '8000-sarasm93-cineramacinema-g8epztv7gsc.ws-eu101.gitpod.io']
                                                     
