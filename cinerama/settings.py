a35de234b7a37da4753632f7220840b53b9ebd5b
Sara Smith <saraa.smith@hotmail.com>
2023-07-14 02:44:25 +0000

    Change debug mode to false

--- cinerama/settings.py
+++ cinerama/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'cinerama-cinema-4c2cf705bc92.herokuapp.com',
